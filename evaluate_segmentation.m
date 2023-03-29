ab23c05cbe0060ea405d89e53fdd358acf46fc8f
ChmelJ <chmelikj@vutbr.cz>
2023-03-29 22:35:04 +0200

    eval update
    Signed-off-by: ChmelJ <chmelikj@vutbr.cz>

--- evaluate_segmentation.m
+++ evaluate_segmentation.m
@@ -1,13 +1,13 @@
 function [dice_scores] = evaluate_segmentation(gt_path, segmented_path)
-gt_mask_names = dir([gt_path '\mask*.png']);
+segmented_mask_names = dir([segmented_path '\mask*.png']);
 dice_scores.details = [];
 images_names = {};
 values = [0,1,2,3,4];
 catnames = {'background', 'tumour', 'stroma', 'necrosis', 'fat'};
-for num_mask = 1:length(gt_mask_names)
-    images_names{num_mask} = gt_mask_names(num_mask).name(end-5:end-4);
-    gt = categorical(imread([gt_path '\' gt_mask_names(num_mask).name]), values, catnames);
-    segmented = categorical(imread([segmented_path '\' gt_mask_names(num_mask).name]), values, catnames);
+for num_mask = 1:length(segmented_mask_names)
+    images_names{num_mask} = segmented_mask_names(num_mask).name(end-5:end-4);
+    gt = categorical(imread([gt_path '\' segmented_mask_names(num_mask).name]), values, catnames);
+    segmented = categorical(imread([segmented_path '\' segmented_mask_names(num_mask).name]), values, catnames);
     dice_scores.details(1:5,num_mask) = dice(segmented,gt);
 end
 dice_scores.mean_dice = nanmean(dice_scores.details(:));
